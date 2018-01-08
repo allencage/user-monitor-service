9ea5604c17c8f01f005a16dcadb5215f777067d2
aciuca <alingabriel.ciuca@gmail.com>
2018-01-08 11:46:20 +0200

    Added logic as .net standard

---
+++ Service/Service1.cs
@@ -0,0 +1,28 @@
+﻿using Logic;
+using System.ServiceProcess;
+
+namespace Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            CanHandlePowerEvent = true;
+            CanHandleSessionChangeEvent = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            UserActions.LogUserAction(changeDescription);
+        }
+    }
+}
